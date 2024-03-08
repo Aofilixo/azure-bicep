6f9c7df7be16ecab9d9621387a8802f382ab477e
Alex <alexofili@me.com>
2024-03-07 18:58:08 -0800

    (maint) updating ugly

---
+++ PaloBicepTemplate/validate.sh
@@ -0,0 +1,12 @@
+# Get the current date in the format MM-dd-yyyy
+date=$(date +%m-%d-%Y)
+
+# Concatenate the deployment name with the current date
+deploymentName="AzPaloDeployment$date"
+
+# Deploy the resources using the Bicep file and parameter file
+az deployment group validate \
+  --name "$deploymentName" \
+  --resource-group rg-cloudcore-egress-vdi-eastus-sandbox \
+  --template-file ./main.bicep \
+  --parameters @main.parameters.json
